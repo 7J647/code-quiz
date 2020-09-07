4056726494ef89a2a09ebbe72e524b28dba597ff
7J647 <007J647@gmail.com>
2020-09-07 16:57:49 -0400

    added high score text in upper right, created container for the questions and intro text and used padding to add margin from the top

--- script.js
+++ script.js
@@ -11,29 +11,52 @@
 //8)  if all questions answered or timer = 0 end game
 //9)  user has option to save initials and store score
 
-//create the timer
-var timer = document.createElement ("div");
-//add content
-timer.textContent = "Time Remaining = 60 seconds";
-timer.setAttribute("style", "font-size:medium; text-align:right; margin-right:20px;");
+//create the high score link and timer container
+var scoreTimer = document.createElement ("div");
+    
+    //add highscore content
+    var highScore = document.createElement ("div");
+    highScore.textContent = "View High Scores";
+    highScore.setAttribute("style", "font-size:medium; text-align:left; margin-left:20px; float:left;")
+    //append to container
+    scoreTimer.appendChild(highScore);
+
+    //add the timer
+    var timer = document.createElement ("div");
+    //add content
+    timer.textContent = "Time Remaining = 60 seconds";
+    timer.setAttribute("style", "font-size:medium; text-align:right; margin-right:20px; float:right;")
+    //append to container
+    scoreTimer.appendChild(timer);
+
 //append to body
-document.body.appendChild(timer);
+document.body.appendChild(scoreTimer);
 
-//create welcome header
-var welcomeHeader = document.createElement ("h1");
-//add content
-welcomeHeader.textContent = "Welcome to the Quiz!";
-//append and style
-document.body.appendChild (welcomeHeader);
-welcomeHeader.setAttribute("style", "text-align:center");
 
-//create intro text
-var introText = document.createElement ("p");
-//add content with instructions
-introText.textContent = "This is where all of the instructions will go";
-//append and style
-document.body.appendChild (introText);
-introText.setAttribute("style", "text-align:center");
+// // //create questions/intro text container
+var questionsIntro = document.createElement ("div");
+//add padding to gap it down from the high score and timer
+questionsIntro.setAttribute("style", "padding:50px;");
+
+    //create welcome header/questions text
+    var question = document.createElement ("h1");
+    //add content and style
+    question.textContent = "Welcome to the Quiz!";
+    question.setAttribute("style", "text-align:center;");
+
+    //append to questions/intro text container
+    questionsIntro.appendChild (question);
+
+    //create intro text
+    var introText = document.createElement ("p");
+    //add content with instructions and style
+    introText.textContent = "This is where all of the instructions will go";
+    introText.setAttribute("style", "text-align:center");
+    //append and style
+    questionsIntro.appendChild (introText);
+
+//append to body
+document.body.appendChild(questionsIntro);
 
 //create container to center button
 var buttonContainer = document.createElement("div");
@@ -54,10 +77,11 @@ document.body.appendChild(buttonContainer);
 //add event listener
 startButton.addEventListener("click", function() {
     //remove welcome headline, instructions, and button
-    welcomeHeader.textContent = "";
+    question.textContent = "";
     introText.textContent = "";
     startButton.setAttribute("style", "visibility:hidden")
     setTime()
+    questionOne()
 })
 
 //create function to start timer countdown
@@ -75,6 +99,82 @@ function setTime() {
   
     }, 1000);
   }
+
+  function questionOne() {
+      question.textContent = "This is question #1";
+      question.setAttribute("style", "margin-left: 300px; text-align:left;"); 
+  }
+
+
+// //create a container to hold the first question
+// var questionContainer = document.createElement ("div");
+
+// //add content
+
+//     var questionOne = document.createElement ("h1");
+
+
+
+
+//create function to start quiz
+
+// function getStarted() {
+//     //remove welcome headline and instructions
+//     welcomeHeader.textContent = "";
+//     introText.textContent = "";
+    
+//     //create first question
+//     var firstQuestion = document.createElement("h1");
+    
+//     //add content
+//     firstQuestion.textContent = "Commonly used data types do NOT include"
+//     firstQuestion.setAttribute ("style, margin-left: 33.3%")
+
+// }
+
+//create function to start timer countdown
+
+var secondsLeft = 60
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timer.textContent = "Time Remaining = " + secondsLeft + " seconds";
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+      }
+  
+    }, 1000);
+  }
+
+
+
+    // //create container to hold question and buttons, roughtly 1/3 over from the left
+    // var questionContainer = document.createElement("div");
+    // //add content
+    // buttonContainer.setAttribute("style", "margin-left:33.3%");
+    //     //create question
+    //     var codeQuestions = document.createElement "h1";
+    //     //add content to question
+    //     codeQuestions.textContent = "This is the first question of the quiz";
+    //     //append to the container
+    //     questionContainer.appendChild(codeQuestions);
+    // //append container to document
+    // document.body.appendChild(questionContainer);
+
+        //create question 
+
+
+
+// //create welcome header
+// var welcomeHeader = document.createElement ("h1");
+// //add content
+// welcomeHeader.textContent = "Welcome to the Quiz!";
+// //append and style
+// document.body.appendChild (welcomeHeader);
+// welcomeHeader.setAttribute("style", "text-align:center");
+
 
 
 // //create a container to hold the first question
