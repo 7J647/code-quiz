3ea35d89d77359635a139109bbe3f56021230a5a
7J647 <007J647@gmail.com>
2020-09-07 12:08:15 -0400

    added pseudocode to script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("hello world");
+
+//pseudocode
+//1) welcome user and present rules
+//2) click start button and timer countdown begins
+//3)  question presented (multiple choice)
+//4)  question answered 
+//5)  event listener to see which one answered
+//6)  if incorrect subtract time from clock
+//7)  else present next question and repeat 4-6 above
+//8)  if all questions answered or timer = 0 end game
+//9)  user has option to save initials and store score
